ed9b54d0330ff9e9be28cd4f407ce95ce9233138
Atharva <atharvagawas21@gmail.com>
2024-10-19 09:07:00 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -64,11 +64,25 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 
 
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
 # ... place you code here to READ an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -88,12 +102,36 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 
 
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def test_update_account(self):
+    """It should Update an existing Account"""
+    # create an Account to update
+    test_account = AccountFactory()
+    resp = self.client.post(BASE_URL, json=test_account.serialize())
+    self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    # update the account
+    new_account = resp.get_json()
+    new_account["name"] = "Something Known"
+    resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    updated_account = resp.get_json()
+    self.assertEqual(updated_account["name"], "Something Known")
+
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
 
+
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def test_delete_account(self):
+    """It should Delete an Account"""
+    account = self._create_accounts(1)[0]
+    resp = self.client.delete(f"{BASE_URL}/{account.id}")
+    self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
